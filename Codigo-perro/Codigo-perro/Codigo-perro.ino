2ddfce32d5bd5b96aab30eb8abef7b4173911ba7
JuanRomanOtero10 <48417781@est.ort.edu.ar>
2025-08-22 11:22:16 -0300

    Initial commit

---
+++ Codigo-perro/Codigo-perro/Codigo-perro.ino
@@ -0,0 +1,14 @@
+//Grupo 4 Otero - Fraccaro - Ponczyk - Nisim
+
+#define MQ2_PIN 35  
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
